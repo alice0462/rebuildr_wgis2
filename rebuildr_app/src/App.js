ee42a131c30d14d27a7fa823c2d2fcc37cab8e49
Theo <46526935+iamTheo20@users.noreply.github.com>
2025-04-15 13:11:43 +0200

    merge between alice and theo

--- rebuildr_app/src/App.js
+++ rebuildr_app/src/App.js
@@ -5,7 +5,32 @@ import React, { useState, useEffect } from 'react';
 
 function HomePage() {
   const [storedRating, setStoredRating] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0); // Track current user index
 
+  useEffect(() => {
+    fetch('/data/userData.json')
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error('Error loading user data:', err));
+  }, []);
+  const handleNextUser = () => {
+    // Move to next index, wrap around if at end
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % users.length);
+  };
+  const user = users[currentIndex];
+
+  let medalSrc = '';
+  if (user) {
+    const co2Value = parseInt(user.totalCo2Saved); // extracts number from "435 kg"
+    if (co2Value < 300) {
+      medalSrc = '/images/bronze-medal2.svg';
+    } else if (co2Value < 700) {
+      medalSrc = '/images/silver-medal2.svg';
+    } else {
+      medalSrc = '/images/gold-medal2.svg';
+    }
+  }
   useEffect(() => {
     const rating = localStorage.getItem("avgRating");
     setStoredRating(rating);
@@ -13,21 +38,24 @@ function HomePage() {
   
   return (
     <div className="app">
+      <button onClick={handleNextUser}>Next User</button>
       <div id = "background"> 
         <div id="profile-pic">
           Profile pic
         </div>
         <div id="user-name">
-          User name
+        <p>{user ? user.name : 'Loading or user not found'}</p>
         </div>
         
         <div id="body">
           <div className="stats">
             <div id="rank">
-              Rank
+            {user && (
+              <img src={medalSrc} alt="user rank medal" id="medal" />
+             )}
             </div>
             <div id="co2">
-              Emission
+            <p>{user ? user.totalCo2Saved : 'Loading or user not found'}</p>
             </div>
 
               <div id="reviews">
